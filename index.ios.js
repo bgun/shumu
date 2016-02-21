6dac726d7de19fec67213fccc1f2eb044db03be0
Ben Gundersen <ben@bengundersen.com>
2016-02-21 02:48:58 -0500

    folder structure

--- index.ios.js
+++ index.ios.js
@@ -1,47 +1,7 @@
 'use strict';
 
-import React  from 'react-native';
+import React from 'react-native';
 
-import Dimensions from 'Dimensions';
+import shumu from './src/app';
 
-import CardContainer from './CardContainer';
-
-let {
-  Animated,
-  AppRegistry,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-} = React;
-
-var ww = Dimensions.get('window').width;
-var wh = Dimensions.get('window').height;
-
-console.log(wh, ww);
-
-
-class shumu extends React.Component {
-  render() {
-    return (
-      <View style={ styles.mainView }>
-        <CardContainer />
-      </View>
-    );
-  }
-}
-
-let styles = StyleSheet.create({
-  mainView: {
-    backgroundColor: '#332255',
-    flex: 1
-  },
-  menuView: {
-    backgroundColor: '#FF0000',
-    height: wh,
-    position: 'absolute',
-    width: ww
-  },
-});
-
-AppRegistry.registerComponent('shumu', () => shumu);
+React.AppRegistry.registerComponent('shumu', () => shumu);
---
+++ src/app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+import React  from 'react-native';
+
+import Dimensions from 'Dimensions';
+
+import CardContainer from './CardContainer';
+
+let {
+  Animated,
+  AppRegistry,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  } = React;
+
+var ww = Dimensions.get('window').width;
+var wh = Dimensions.get('window').height;
+
+console.log(wh, ww);
+
+
+export default class shumu extends React.Component {
+  render() {
+    return (
+      <View style={ styles.mainView }>
+        <CardContainer />
+      </View>
+    );
+  }
+}
+
+let styles = StyleSheet.create({
+  mainView: {
+    backgroundColor: '#332255',
+    flex: 1
+  },
+  menuView: {
+    backgroundColor: '#FF0000',
+    height: wh,
+    position: 'absolute',
+    width: ww
+  },
+});
+
